3a83cf214b16d77381e2ab8572d5c185f785a52c
Kai <Kaivandenbroek@hotmail.com>
2020-09-01 23:35:21 +0200

    almost done

--- app/src/main/java/com/example/madlevel1task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel1task2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.madlevel1task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var correctsAnswers = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +28,28 @@ class MainActivity : AppCompatActivity() {
     Checks the textfields for the answers
      */
     private fun checkAnswers() {
-        val answer1 = binding.answer1.text.toString()
-        val answer2 = binding.answer2.text.toString()
-        val answer3 = binding.answer3.text.toString()
-        val answer4 = binding.answer4.text.toString()
+        val answer1 = binding.answer1.text
+        val answer2 = binding.answer2.text
+        val answer3 = binding.answer3.text
+        val answer4 = binding.answer4.text
 
+        if (answer1.equals(getString(R.string.T))) {
+            correct()
+        }
+        if (answer2.equals(getString(R.string.F))) {
+            correct()
+        }
+        if (answer3.equals(getString(R.string.F))) {
+            correct()
+        }
+        if (answer4.equals(getString(R.string.F))) {
+            correct()
+        }
+        Toast.makeText(this, getString(R.string.correct, correctsAnswers), correctsAnswers).show()
+
+    }
+
+    private fun correct() {
+        correctsAnswers++
     }
 }
